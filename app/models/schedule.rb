697024d89f49ba47552d78b68fccdce888c2d8e0
I_Kenya <kenya.imaizumi.prog@gmail.com>
2023-04-06 23:49:11 +0900

    validates の追記

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,2 +1,6 @@
 class Schedule < ApplicationRecord
+  validates :title, presence :true, length {maximum: 20}
+  validates :start, presence :true
+  validates :end, presence :true, comparison: {greater_than: :start}
+  validates :memo, presence :true, length {maximum: 500}
 end
